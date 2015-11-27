0560449f1f6371a54c103d650f54790d3d786b41
Suresh Sundriyal <sureshsundriyal@gmail.com>
2015-11-26 20:17:12 -0800

    [setup.py] Change to Alpha status.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+#from setuptools import setup
 setup(name='pysqlitebkup',
       version='0.0.1',
       py_modules=['pysqlitebkup'],
@@ -7,11 +8,11 @@ setup(name='pysqlitebkup',
       url='https://github.com/sureshsundriyal/pysqlitebkup',
       keywords=['sqlite3', 'backup', 'online backup', 'pure python'],
       license="CC0 - No rights reserved.",
-      description=('A pure python library that exposes the online sqlite3 '
+      description=('A pure python library that exposes the sqlite3 online '
                    'backup functions.'),
       classifiers=[
           'Topic :: Database',
-          'Development Status :: 4 - Beta',
+          'Development Status :: 3 - Alpha',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3',
           'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
